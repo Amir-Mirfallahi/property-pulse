25c76c19fbcf39748d6b8c6a0114c7edaaae19f7
Amir Mirfallahi <mirfallahi2009@gmail.com>
2024-04-09 21:32:10 +0330

    End of Chapter 2: Creating HomePage, PropertiesPage, NotFoundPage, LoadingPage, added react spinners, properties.json(Temp Data)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,23 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
-    title: "PropertyPuls | Find The Perfect Rental",
-    description: "Find your dream rental property",
-    keywords: "rental, find rentals, find properties",
+  title: "PropertyPuls | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
 };
 
 const MainLayout = ({ children }) => {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 };
 
 export default MainLayout;
