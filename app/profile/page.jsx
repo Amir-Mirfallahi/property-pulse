48a72280330b3109a1b43f64348a23a1d49476f6
Amir Mirfallahi <mirfallahi2009@gmail.com>
2024-04-16 21:50:28 +0330

    Added delete properties functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -41,7 +41,30 @@ const ProfilePage = () => {
     }
   }, [session]);
 
-  const handleDeleteProperty = (e) => {};
+  const handleDeleteProperty = async (propertyId) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this property? "
+    );
+    if (!confirmed) return;
+
+    try {
+      const res = await fetch(`/api/properties/${propertyId}`, {
+        method: "DELETE",
+      });
+      if (res.status === 200) {
+        // Remove property from state
+        const updatedProperties = properties.filter(
+          (property) => property._id !== propertyId
+        );
+        setProperties(updatedProperties);
+        alert("Property Deleted");
+      } else {
+        alert("failed to delete property");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <section className="bg-blue-50">
