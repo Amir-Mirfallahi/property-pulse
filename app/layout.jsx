59f375831505ee2a325c218fe4923f95d6ad0e5a
Amir Mirfallahi <mirfallahi2009@gmail.com>
2024-04-17 22:45:58 +0330

    Created put request management in /api/properties/:id

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,11 +4,13 @@ import AuthProvider from "@/components/AuthProvider";
 import "@/assets/styles/globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import logo from "@/assets/images/logo.png";
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
   description: "Find your dream rental property",
   keywords: "rental, find rentals, find properties",
+  favicon: logo,
 };
 
 const MainLayout = ({ children }) => {
